a425d21a96c16abefa7010b7b0cefde60aabf14d
Réna Hajjar <renashajjar@gmail.com>
2025-03-11 21:22:47 -0400

    add read me and load env

--- outlook_auth.py
+++ outlook_auth.py
@@ -1,5 +1,10 @@
 from O365 import Account
 import os
+from dotenv import load_dotenv
+import requests
+import json
+
+load_dotenv()
 
 credentials = os.getenv('CLIENT_ID'), os.getenv('CLIENT_SECRET')
 
@@ -7,3 +12,14 @@ account = Account(credentials)
 account.authenticate(scopes=['basic', 'message_all'])
 
 mailbox = account.mailbox()
+
+
+def try_getting():
+    request = requests.get("https://graph.microsoft.com/v1.0/me/drive/root/children")
+    print("get something: ")
+    print("status code response: ", request.status_code)
+    print("content response: ", request.content)
+    all_fields = json.loads(request.content)
+    print(all_fields)
+
+try_getting()
---
+++ readme.md
@@ -0,0 +1,5 @@
+## To Run:
+### Set the following variables in your .env:
+
+CLIENT_ID
+CLIENT_SECRET_KEY
